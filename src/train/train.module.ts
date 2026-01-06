0ab2fc884e89082014f7762a9200634a6e988985
junseokhub <github.yam117@passinbox.com>
2026-01-06 15:08:33 +0900

    Fix: remove nestjs

--- src/train/train.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { TrainService } from "./train.service";
-import { TrainParseService } from "./train-parse.service";
-
-@Module({
-  providers: [TrainService, TrainParseService],
-})
-export class TrainModule {}
