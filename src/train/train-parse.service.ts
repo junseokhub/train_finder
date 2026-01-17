969c58fd0969625dccdaa6840827410a925f373d
junseokhub <github.yam117@passinbox.com>
2026-01-17 09:29:11 +0900

    Fix: find station

--- src/train/train-parse.service.ts
+++ src/train/train-parse.service.ts
@@ -20,40 +20,37 @@ export class TrainParseService {
     throw Error('The only valid train names: ktx, srt, normal');
   }
 
- async trainDepArrFinder(depName: string, arrName: string) {
-  const depNode = await this.findStation(depName);
-  const arrNode = await this.findStation(arrName);
+  async trainDepArrFinder(depName: string, arrName: string) {
+    const depNode = await this.findStation(depName);
+    const arrNode = await this.findStation(arrName);
 
-  if (!depNode) {
-    console.log(`Departure station "${depName}" could not be found. Please check the station name again.`);
-  }
-
-  if (!arrNode) {
-    console.log(`Arrival station "${arrName}" could not be found. Please check the station name again.`);
-  }
-
-  const depNodeId = depNode.nodeid;
-  const arrNodeId = arrNode.nodeid;
-
-  return {
-    depNodeId,
-    arrNodeId,
-  };
-}
-
-async findStation(station: String) {
-  for (const region of trainData.nodeList) {
-    const regionKey = Object.keys(region)[0];
-    const nodes = region[regionKey];
-
-    const foundStation = nodes.find(e => e.nodename.toLowerCase().trim() === station.toLowerCase().trim());
-
-    if (foundStation) {
-      return foundStation;
+    if (!depNode) {
+      console.log(`Departure station "${depName}" could not be found. Please check the station name again.`);
     }
+
+    if (!arrNode) {
+      console.log(`Arrival station "${arrName}" could not be found. Please check the station name again.`);
+    }
+
+    const depNodeId = depNode.nodeid;
+    const arrNodeId = arrNode.nodeid;
+
+    return {
+      depNodeId,
+      arrNodeId,
+    };
   }
-  return null;
-}
+
+  findStation(station: string) {
+    const keyword = station.toLowerCase().trim();
+
+    return (
+      trainData.nodeList
+        .flatMap(region => Object.values(region).flat())
+        .find(node => node.nodename.toLowerCase().trim() === keyword)
+      || null
+    );
+  }
 
   stationList() {
     return trainData.nodeList.flatMap(region =>
